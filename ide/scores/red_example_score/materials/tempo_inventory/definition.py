b928fce330f91cacb57038af72119925f5fa15f7
Trevor Bača <trevorbaca@gmail.com>
2015-07-03 12:09:10 -0400

    Updated Red Example Score tempo inventory.

--- ide/scores/red_example_score/materials/tempo_inventory/definition.py
+++ ide/scores/red_example_score/materials/tempo_inventory/definition.py
@@ -5,19 +5,19 @@ from abjad import *
 tempo_inventory = indicatortools.TempoInventory(
     [
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=72,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=108,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=90,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=135,
             ),
         ]
--- ide/scores/red_example_score/materials/tempo_inventory/output.py
+++ ide/scores/red_example_score/materials/tempo_inventory/output.py
@@ -6,19 +6,19 @@ from abjad.tools import indicatortools
 tempo_inventory = indicatortools.TempoInventory(
     [
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=72,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=108,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=90,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 8),
+            reference_duration=durationtools.Duration(1, 8),
             units_per_minute=135,
             ),
         ]
