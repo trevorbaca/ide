c364a29769d7dd72f45141e9c52cf9694f77e05b
Trevor Bača <trevorbaca@gmail.com>
2015-07-07 10:12:19 -0400

    Removed test file.

--- ide/idetools/test/test_MaterialPackageWrangler__get_available_path.py
+++ ide/idetools/test/test_MaterialPackageWrangler__get_available_path.py
@@ -8,25 +8,6 @@ configuration = ide.idetools.Configuration()
 def test_MaterialPackageWrangler__get_available_path_01():
 
     session = ide.idetools.Session(is_test=True)
-    session._set_test_score('red_example_score')
-    wrangler = ide.idetools.MaterialPackageWrangler(session=session)
-    input_ = 'foo'
-    wrangler._session._pending_input = input_
-    result = wrangler._get_available_path()
-    path = os.path.join(
-        configuration.example_score_packages_directory,
-        'red_example_score',
-        'red_example_score',
-        'materials',
-        'foo',
-        )
-
-    assert result == path
-
-
-def test_MaterialPackageWrangler__get_available_path_02():
-
-    session = ide.idetools.Session(is_test=True)
     wrangler = ide.idetools.MaterialPackageWrangler(session=session)
 
     input_ = 'q'
