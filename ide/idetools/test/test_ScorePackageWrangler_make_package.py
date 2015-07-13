cd64d685e4f737adac391a8d1a279e856836e142
Trevor Bača <trevorbaca@gmail.com>
2015-07-13 09:27:37 -0400

    Updated package creation test.
    Test now cleans up score package correctly during tear down.

--- ide/idetools/test/test_ScorePackageWrangler_make_package.py
+++ ide/idetools/test/test_ScorePackageWrangler_make_package.py
@@ -9,11 +9,11 @@ def test_ScorePackageWrangler_make_package_01():
     r'''Makes score package.
     '''
 
-    path = os.path.join(
+    outer_path = os.path.join(
         abjad_ide._configuration.user_score_packages_directory,
         'example_score',
-        'example_score',
         )
+    inner_path = os.path.join(outer_path, 'example_score')
     directory_entries = [
         '__init__.py',
         '__metadata__.py',
@@ -25,14 +25,14 @@ def test_ScorePackageWrangler_make_package_01():
         'stylesheets',
         ]
 
-    with systemtools.FilesystemState(remove=[path]):
+    with systemtools.FilesystemState(remove=[outer_path]):
         input_ = 'new example~score y q'
         abjad_ide._run(input_=input_)
         contents = abjad_ide._transcript.contents
-        assert os.path.exists(path)
+        assert os.path.exists(outer_path)
         session = ide.idetools.Session(is_test=True)
         manager = ide.idetools.ScorePackageManager
-        manager = manager(path=path, session=session)
+        manager = manager(path=inner_path, session=session)
         assert manager._list() == directory_entries
 
     assert 'Enter score package name]>' in contents
