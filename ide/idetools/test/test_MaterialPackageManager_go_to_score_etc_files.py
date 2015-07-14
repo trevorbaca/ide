db0ed3e9ce23de1b3a2d300470379d61f63ac512
Trevor Bača <trevorbaca@gmail.com>
2015-07-14 11:30:10 -0400

    Wrote (e) and (ee) tests.
    This closes GitHub #21.

---
+++ ide/idetools/test/test_MaterialPackageManager_go_to_score_etc_files.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import ide
+abjad_ide = ide.idetools.AbjadIDE(is_test=True)
+
+
+def test_MaterialPackageManager_go_to_score_etc_files_01():
+
+    input_ = 'red~example~score m tempo~inventory e q'
+    abjad_ide._run(input_=input_)
+
+    titles = [
+        'Abjad IDE - scores',
+        'Red Example Score (2013)',
+        'Red Example Score (2013) - materials directory',
+        'Red Example Score (2013) - materials directory - tempo inventory',
+        'Red Example Score (2013) - etc directory',
+        ]
+    assert abjad_ide._transcript.titles == titles
