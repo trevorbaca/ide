1966d5ff58f61aaa4830c22f38185cde94225c23
Trevor Bača <trevorbaca@gmail.com>
2015-07-31 09:33:01 -0400

    Added Command._allowable_menu_section_names class variable.

--- ide/tools/idetools/Command.py
+++ ide/tools/idetools/Command.py
@@ -6,20 +6,36 @@ class Command(object):
     r'''Command decorator.
     '''
 
+    ### CLASS VARIABLES ###
+
+    _allowable_menu_section_names = (
+        'basic operation',
+        'build',
+        'cross-comparison',
+        'git',
+        'navigation',
+        'package',
+        'score-global',
+        'star',
+        'system',
+        )
+
     ### INITIALIZER ###
 
     def __init__(
         self, 
         command_name, 
         description, 
-        menu_section,
+        menu_section_name,
         is_navigation=False,
         ):
         assert isinstance(command_name, str), repr(command_name)
         assert Command._is_valid_command_name(command_name), repr(command_name)
         self.command_name = command_name
         self.description = description
-        self.menu_section = menu_section
+        assert menu_section_name in self._allowable_menu_section_names, repr(
+            menu_section_name)
+        self.menu_section_name = menu_section_name
         self.is_navigation = is_navigation
 
     ### SPECIAL METHODS ###
@@ -31,7 +47,7 @@ class Command(object):
         '''
         method.command_name = self.command_name
         method.description = self.description
-        method.menu_section = self.menu_section
+        method.menu_section_name = self.menu_section_name
         method.is_navigation = self.is_navigation
         return method
 
