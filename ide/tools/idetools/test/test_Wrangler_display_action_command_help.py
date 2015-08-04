e9e03c129f16abd87b865a2864f8f96c90b1a74d
Trevor Bača <trevorbaca@gmail.com>
2015-08-04 10:42:57 -0400

    Wrote complete menu tests for (?).

--- ide/tools/idetools/test/test_Wrangler_display_action_command_help.py
+++ ide/tools/idetools/test/test_Wrangler_display_action_command_help.py
@@ -50,3 +50,197 @@ def test_Wrangler_display_action_command_help_04():
     first_real_line = '    display action command help (?)'
     string = '\n'.join([title, first_blank_line, first_real_line])
     assert string in contents
+
+
+def test_Wrangler_display_action_command_help_05():
+    r'''Works at home screen.
+    '''
+
+    lines = [
+        'Abjad IDE - all score directories - action commands',
+        '',
+        '    display action command help (?)',
+        '    invoke shell (!)',
+        '',
+        '    open lilypond log (log)',
+        '',
+        '    check every package (ck*)',
+        '    open every score pdf (so*)',
+        '',
+        '    git add every package (add*)',
+        '    git commit every package (ci*)',
+        '    git revert every package (revert*)',
+        '    git status every package (st*)',
+        '    git update every package (up*)',
+        '',
+        '    copy (cp)',
+        '    new (new)',
+        '    remove (rm)',
+        '    rename (ren)',
+        '',
+        '>',
+        ]
+
+    input_ = '? q'
+    abjad_ide._run(input_=input_)
+    transcript_entry = abjad_ide._session._transcript.entries[-3]
+
+    for line, actual_line in zip(lines, transcript_entry.lines):
+        assert line == actual_line
+
+
+def test_Wrangler_display_action_command_help_06():
+    r'''Works at all materials directories.
+    '''
+
+    lines = [
+        'Abjad IDE - all materials directories - action commands',
+        '',
+        '    display action command help (?)',
+        '    invoke shell (!)',
+        '',
+        '    open lilypond log (log)',
+        '',
+        '    copy (cp)',
+        '    new (new)',
+        '    remove (rm)',
+        '    rename (ren)',
+        '',
+        '>',
+        ]
+
+    input_ = 'mm ? q'
+    abjad_ide._run(input_=input_)
+    transcript_entry = abjad_ide._session._transcript.entries[-3]
+
+    for line, actual_line in zip(lines, transcript_entry.lines):
+        assert line == actual_line
+
+
+
+def test_Wrangler_display_action_command_help_07():
+    r'''Works at all stylesheets directories.
+    '''
+
+    lines = [
+        'Abjad IDE - all stylesheets directories - action commands',
+        '',
+        '    display action command help (?)',
+        '    invoke shell (!)',
+        '',
+        '    open lilypond log (log)',
+        '',
+        '    copy (cp)',
+        '    new (new)',
+        '    remove (rm)',
+        '    rename (ren)',
+        '',
+        '>',
+        ]
+
+    input_ = 'yy ? q'
+    abjad_ide._run(input_=input_)
+    transcript_entry = abjad_ide._session._transcript.entries[-3]
+
+    for line, actual_line in zip(lines, transcript_entry.lines):
+        assert line == actual_line
+
+
+def test_Wrangler_display_action_command_help_08():
+    r'''Works in materials directory.
+    '''
+
+    lines = [
+        'Red Example Score (2013) - materials directory - action commands',
+        '',
+        '    display action command help (?)',
+        '    invoke shell (!)',
+        '',
+        '    edit abbreviations file (abb)',
+        '    edit score stylesheet (sty)',
+        '    open lilypond log (log)',
+        '',
+        '    check every definition py (dc*)',
+        '    edit every definition py (de*)',
+        '    interpret every illustration ly (ii*)',
+        '    open every illustration pdf (io*)',
+        '',
+        '    copy (cp)',
+        '    new (new)',
+        '    remove (rm)',
+        '    rename (ren)',
+        '',
+        '>',
+        ]
+
+    input_ = 'red~example~score m ? q'
+    abjad_ide._run(input_=input_)
+    transcript_entry = abjad_ide._session._transcript.entries[-3]
+
+    for line, actual_line in zip(lines, transcript_entry.lines):
+        assert line == actual_line
+
+
+def test_Wrangler_display_action_command_help_09():
+    r'''Works in segments directory.
+    '''
+
+    lines = [
+        'Red Example Score (2013) - segments directory - action commands',
+        '',
+        '    display action command help (?)',
+        '    invoke shell (!)',
+        '',
+        '    edit abbreviations file (abb)',
+        '    edit score stylesheet (sty)',
+        '    open lilypond log (log)',
+        '',
+        '    check every definition py (dc*)',
+        '    edit every definition py (de*)',
+        '    interpret every illustration ly (ii*)',
+        '    open every illustration pdf (io*)',
+        '',
+        '    copy (cp)',
+        '    new (new)',
+        '    remove (rm)',
+        '    rename (ren)',
+        '',
+        '>',
+        ]
+
+    input_ = 'red~example~score g ? q'
+    abjad_ide._run(input_=input_)
+    transcript_entry = abjad_ide._session._transcript.entries[-3]
+
+    for line, actual_line in zip(lines, transcript_entry.lines):
+        assert line == actual_line
+
+
+def test_Wrangler_display_action_command_help_10():
+    r'''Works in stylesheets directory.
+    '''
+
+    lines = [
+        'Red Example Score (2013) - stylesheets directory - action commands',
+        '',
+        '    display action command help (?)',
+        '    invoke shell (!)',
+        '',
+        '    edit abbreviations file (abb)',
+        '    edit score stylesheet (sty)',
+        '    open lilypond log (log)',
+        '',
+        '    copy (cp)',
+        '    new (new)',
+        '    remove (rm)',
+        '    rename (ren)',
+        '',
+        '>',
+        ]
+
+    input_ = 'red~example~score y ? q'
+    abjad_ide._run(input_=input_)
+    transcript_entry = abjad_ide._session._transcript.entries[-3]
+
+    for line, actual_line in zip(lines, transcript_entry.lines):
+        assert line == actual_line
