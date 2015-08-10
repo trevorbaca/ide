760adaa43e7f8f12bd166ad3fc262da630348ba0
Trevor Bača <trevorbaca@gmail.com>
2015-08-10 09:07:50 -0400

    Refactored package creation methods.

--- ide/tools/idetools/PackageManager.py
+++ ide/tools/idetools/PackageManager.py
@@ -22,66 +22,6 @@ class PackageManager(Controller):
         '_required_files',
         )
 
-    _directory_name_to_package_contents = {
-        'materials': {
-            'optional_directories': (
-                '__pycache__',
-                'test',
-                ),
-            'optional_files': (
-                '__illustrate__.py',
-                'illustration.ly',
-                'illustration.pdf',
-                'maker.py',
-                ),
-            'required_directories': (),
-            'required_files': (
-                '__init__.py',
-                '__metadata__.py',
-                'definition.py',
-                ),
-            },
-        'score': {
-            'optional_directories': (
-                '__pycache__',
-                'etc',
-                'test',
-                ),
-            'optional_files': (),
-            'required_directories': (
-                'build',
-                'distribution',
-                'makers',
-                'materials',
-                'segments',
-                'stylesheets',
-                ),
-            'required_files': (
-                '__init__.py',
-                '__metadata__.py',
-                os.path.join('makers', '__init__.py'),
-                os.path.join('materials', '__init__.py'),
-                os.path.join('segments', '__init__.py'),
-                ),
-            },
-        'segments': {
-            'optional_directories': (
-                '__pycache__',
-                'test',
-                ),
-            'optional_files': (
-                'illustration.ly',
-                'illustration.pdf',
-                ),
-            'required_directories': (),
-            'required_files': (
-                '__init__.py',
-                '__metadata__.py',
-                'definition.py',
-                ),
-            },
-        }
-
     ### INITIALIZER ###
 
     def __init__(self, path=None, session=None):
