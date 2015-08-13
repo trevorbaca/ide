87c8e30105ac6740aef8103ac99492d80abbc474
Trevor Bača <trevorbaca@gmail.com>
2015-08-12 22:08:38 -0400

    Removed ControllerContext.current_score_directory property.

--- ide/tools/idetools/ControllerContext.py
+++ ide/tools/idetools/ControllerContext.py
@@ -12,10 +12,8 @@ class ControllerContext(ContextManager):
         '_clear_terminal',
         '_consume_local_backtrack',
         '_controller',
-        '_current_score_directory',
         '_directory_name',
         '_is_in_confirmation_environment',
-        '_old_current_score_directory',
         '_on_enter_callbacks',
         '_on_exit_callbacks',
         '_session',
@@ -28,7 +26,6 @@ class ControllerContext(ContextManager):
         clear_terminal=False,
         consume_local_backtrack=False,
         controller=None,
-        current_score_directory=None,
         directory_name=None,
         is_in_confirmation_environment=False,
         on_enter_callbacks=None,
@@ -37,7 +34,6 @@ class ControllerContext(ContextManager):
         self._clear_terminal = clear_terminal
         self._consume_local_backtrack = consume_local_backtrack
         self._controller = controller
-        self._current_score_directory = current_score_directory
         self._directory_name = directory_name
         self._is_in_confirmation_environment = is_in_confirmation_environment
         self._on_enter_callbacks = on_enter_callbacks or ()
@@ -55,11 +51,6 @@ class ControllerContext(ContextManager):
             self._is_in_confirmation_environment
         if self._clear_terminal:
             self._session._pending_redraw = True
-        if self._current_score_directory is not None:
-            self._old_current_score_directory = \
-                self._session._current_score_directory
-            self._session._current_score_directory = \
-                self._current_score_directory
         for on_enter_callback in self._on_enter_callbacks:
             on_enter_callback(directory_name=self._directory_name)
 
@@ -71,9 +62,6 @@ class ControllerContext(ContextManager):
         self._session._is_in_confirmation_environment = False
         if self._consume_local_backtrack:
             self._session._is_backtracking_locally = False
-        if self._current_score_directory is not None:
-            self._session._current_score_directory = \
-                self._old_current_score_directory
         for on_exit_callback in self._on_exit_callbacks:
             on_exit_callback()
         if self._clear_terminal:
