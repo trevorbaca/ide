b92d5ac8c243a9fdb04060f7bd15f8d9e7e277f2
Trevor Bača <trevorbaca@gmail.com>
2015-09-14 16:35:25 -0400

    Renamed test file.

--- ide/scores/red_example_score/red_example_score/test/test_import.py
+++ ide/scores/red_example_score/red_example_score/test/test_import.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def test_red_example_score_import_01():
+def test_import_01():
     from ide.tools import idetools
     configuration = idetools.AbjadIDEConfiguration()
     configuration._add_example_score_to_sys_path()
