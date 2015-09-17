238286253afc0b004a07bb2a99e55e6c451edb66
Trevor Bača <trevorbaca@gmail.com>
2015-09-17 11:38:40 -0400

    Updated boilerplate.

--- ide/boilerplate/__illustrate_material__.py
+++ ide/boilerplate/__illustrate_material__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-import traceback
 from abjad import *
 from {score_package_name}.materials.{material_package_name}.definition import {material_package_name}
 
@@ -9,16 +6,4 @@ from {score_package_name}.materials.{material_package_name}.definition import {m
 def make_lilypond_file({material_package_name}):
     pass
 
-if __name__ == '__main__':
-    lilypond_file = make_lilypond_file({material_package_name})
-    try:
-        current_directory = os.path.dirname(__file__)
-        candidate_path = os.path.join(
-            current_directory,
-            'illustration.candidate.pdf',
-            )
-        persist(lilypond_file).as_pdf(candidate_path)
-    except:
-        traceback.print_exc()
-        sys.exit(1)
-    sys.exit(0)
+lilypond_file = make_lilypond_file()
