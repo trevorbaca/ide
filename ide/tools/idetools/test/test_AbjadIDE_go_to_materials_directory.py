461dbda7d1e9f281b22085c076c16367c14a328f
Trevor Bača <trevorbaca@gmail.com>
2015-09-25 15:49:20 -0400

    Wrote materials directory view tests. Wrote segments directory view tests.

--- ide/tools/idetools/test/test_AbjadIDE_go_to_materials_directory.py
+++ ide/tools/idetools/test/test_AbjadIDE_go_to_materials_directory.py
@@ -5,7 +5,7 @@ abjad_ide = ide.tools.idetools.AbjadIDE(is_test=True)
 
 
 def test_AbjadIDE_go_to_materials_directory_01():
-    r'''Goes from build directory to materials directory.
+    r'''From build directory.
     '''
 
     input_ = 'red~example~score bb mm q'
@@ -17,3 +17,18 @@ def test_AbjadIDE_go_to_materials_directory_01():
         'Red Example Score (2013) - materials directory',
         ]
     assert abjad_ide._io_manager._transcript.titles == titles
+
+
+def test_AbjadIDE_go_to_materials_directory_02():
+    r'''Makes sure 'inventories first' view is in effect.
+    '''
+
+    input_ = 'red~example~score mm q'
+    abjad_ide._start(input_=input_)
+
+    contents = abjad_ide._io_manager._transcript.contents
+    assert '5: performer inventory' in contents
+    assert '6: pitch range inventory' in contents
+    assert '7: tempo inventory' in contents
+    assert '8: magic numbers' in contents
+    assert '9: time signatures' in contents
--- ide/tools/idetools/test/test_AbjadIDE_go_to_segments_directory.py
+++ ide/tools/idetools/test/test_AbjadIDE_go_to_segments_directory.py
@@ -51,3 +51,15 @@ def test_AbjadIDE_go_to_segments_directory_03():
         'Red Example Score (2013) - segments directory',
         ]
     assert abjad_ide._io_manager._transcript.titles == titles
+
+
+def test_AbjadIDE_go_to_segments_directory_04():
+    r'''Makes sure 'reverse' view is in effect.
+    '''
+
+    input_ = 'blue~example~score gg q'
+    abjad_ide._start(input_=input_)
+    contents = abjad_ide._io_manager._transcript.contents
+
+    assert '4: segment 02' in contents
+    assert '5: segment 01' in contents
