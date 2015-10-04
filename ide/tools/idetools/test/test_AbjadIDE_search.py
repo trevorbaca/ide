4f55a2f41fa40f528fe3c1c454e09e33d3fb0aaa
Trevor Bača <trevorbaca@gmail.com>
2015-10-04 09:35:29 -0400

    Updated 'search' (sr) tests.

--- ide/tools/idetools/test/test_AbjadIDE_search.py
+++ ide/tools/idetools/test/test_AbjadIDE_search.py
@@ -14,13 +14,16 @@ def test_AbjadIDE_search_01():
     abjad_ide._start(input_=input_)
     contents = abjad_ide._io_manager._transcript.contents
 
-    line = 'blue_example_score/blue_example_score/materials/talea_rhythm_maker/definition.py:5:'
-    line += 'talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker('
-    assert line in contents
-    line = 'red_example_score/red_example_score/makers/RhythmMaker.py:5:'
-    line += 'class RhythmMaker(rhythmmakertools.RhythmMaker):'
-    assert line in contents
-
+    ack_line = 'blue_example_score/blue_example_score/materials/talea_rhythm_maker/definition.py:5:'
+    ack_line += 'talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker('
+    grep_line = 'blue_example_score/blue_example_score/materials/talea_rhythm_maker/definition.py:'
+    grep_line += 'talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker('
+    assert ack_line in contents or grep_line in contents
+    ack_line = 'red_example_score/red_example_score/makers/RhythmMaker.py:5:'
+    ack_line += 'class RhythmMaker(rhythmmakertools.RhythmMaker):'
+    grep_line = 'red_example_score/red_example_score/makers/RhythmMaker.py:'
+    grep_line += 'class RhythmMaker(rhythmmakertools.RhythmMaker):'
+    assert ack_line in contents or grep_line in contents
 
 
 def test_AbjadIDE_search_02():
@@ -31,9 +34,13 @@ def test_AbjadIDE_search_02():
     abjad_ide._start(input_=input_)
     contents = abjad_ide._io_manager._transcript.contents
 
-    line = 'blue_example_score/blue_example_score/materials/talea_rhythm_maker/definition.py:5:'
-    line += 'talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker('
-    assert not line in contents
-    line = 'makers/RhythmMaker.py:5:'
-    line += 'class RhythmMaker(rhythmmakertools.RhythmMaker):'
-    assert line in contents
+    ack_line = 'blue_example_score/blue_example_score/materials/talea_rhythm_maker/definition.py:5:'
+    ack_line += 'talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker('
+    grep_line = 'blue_example_score/blue_example_score/materials/talea_rhythm_maker/definition.py:'
+    grep_line += 'talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker('
+    assert not ack_line in contents and not grep_line in contents
+    ack_line = 'makers/RhythmMaker.py:5:'
+    ack_line += 'class RhythmMaker(rhythmmakertools.RhythmMaker):'
+    grep_line = 'makers/RhythmMaker.py:'
+    grep_line += 'class RhythmMaker(rhythmmakertools.RhythmMaker):'
+    assert ack_line in contents or grep_line in contents
