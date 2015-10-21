ef8ae09db95d2893fafc91f9e649752e560710e8
Trevor Bača <trevorbaca@gmail.com>
2015-10-21 11:30:20 -0400

    Wrote (kk) tests.

--- ide/tools/idetools/test/test_AbjadIDE_go_to_makers_directory.py
+++ ide/tools/idetools/test/test_AbjadIDE_go_to_makers_directory.py
@@ -19,6 +19,9 @@ def test_AbjadIDE_go_to_makers_directory_01():
         'Red Example Score (2013) - makers directory',
         ]
     assert abjad_ide._io_manager._transcript.titles == titles
+    contents = abjad_ide._io_manager._transcript.contents
+    assert 'adjust_spacing_sections.py' in contents
+
 
 def test_AbjadIDE_go_to_makers_directory_02():
     r'''From segment directory.
