8f203ab4a3e2039024980cf8d35fc28d6d192f15
Trevor Bača <trevor.baca@gmail.com>
2016-07-13 07:56:19 -0700

    Updated two (new) tests.

--- ide/tools/idetools/test/test_AbjadIDE_new.py
+++ ide/tools/idetools/test/test_AbjadIDE_new.py
@@ -249,7 +249,7 @@ def test_AbjadIDE_new_06():
         assert os.path.exists(path)
         with open(path, 'r') as file_pointer:
             string = file_pointer.read()
-            assert 'class NewMaker(object)' in string
+            assert 'class NewMaker(abjad.abctools.AbjadObject)' in string
 
 
 def test_AbjadIDE_new_07():
@@ -271,7 +271,7 @@ def test_AbjadIDE_new_07():
         assert os.path.exists(path)
         with open(path, 'r') as file_pointer:
             string = file_pointer.read()
-            assert 'class NewMaker(object)' in string
+            assert 'class NewMaker(abjad.abctools.AbjadObject)' in string
 
 
 def test_AbjadIDE_new_08():
