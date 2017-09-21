5a89bacc3447d26c1b599da0ceff6978dfc8403a
Trevor Bača <trevor.baca@gmail.com>
2017-09-21 11:15:58 -0500

    Wrote external directory test. Closes #226.

--- ide/tools/idetools/test/test_AbjadIDE_go_to_external_directory.py
+++ ide/tools/idetools/test/test_AbjadIDE_go_to_external_directory.py
@@ -12,3 +12,10 @@ def test_AbjadIDE_go_to_external_directory_01():
     transcript = abjad_ide.io.transcript
     assert '/Users/trevorbaca/Desktop' in transcript
     assert '.gitignore' not in transcript
+
+    # regression: underscore files appear in external directory
+    abjad_ide('cdj .. boiler q')
+    transcript = abjad_ide.io.transcript
+    assert '/Users/trevorbaca/abjad/abjad/boilerplate' in transcript
+    assert '.gitignore' not in transcript
+    assert '__aliases__.py' in transcript
