15bc17acf1d4f65e0af6cbebdf7151180679713a
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 12:21:15 -0500

    Cleaned up setup.py file.

--- setup.py
+++ setup.py
@@ -14,18 +14,31 @@ author_email = [
     ]
 author_email = ', '.join(author_email)
 
-install_requires = ('abjad',)
+install_requires = [
+    'abjad',
+    'roman',
+    'uqbar>=0.2.13',
+    ]
 
-setuptools.setup(
-    author=author,
-    author_email=author_email,
-    include_package_data=True,
-    install_requires=[
-        'abjad',
-        ],
-    license='GPL',
-    name='Abjad IDE',
-    packages=('ide',),
-    platforms='Any',
-    url='http://www.projectabjad.org',
-    )
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
+if __name__ = '__main__':
+    setuptools.setup(
+        author=author,
+        author_email=author_email,
+        include_package_data=True,
+        install_requires=install_requires,
+        keywords=keywords,
+        license='MIT',
+        name='Abjad IDE',
+        packages=['ide'],
+        platforms='Any',
+        url='http://www.projectabjad.org',
+        )
