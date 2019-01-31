3492fafdb9bfe890473aebe349145289ceedb6ae
Trevor Bača <trevor.baca@gmail.com>
2019-01-31 15:13:12 -0600

    Cleaned up Git comands.

--- ide/test/test_AbjadIDE_git_commit.py
+++ ide/test/test_AbjadIDE_git_commit.py
@@ -11,47 +11,47 @@ def test_AbjadIDE_git_commit_01():
 
     abjad_ide('red ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red bb ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red dd ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red ee ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red oo ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red mm ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red mm rpc ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red gg ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red gg A ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red yy ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
     abjad_ide('red tt ci q')
     transcript = abjad_ide.io.transcript
-    assert f'Nothing to commit ...' in transcript
+    assert '> ci' in transcript
 
 
 def test_AbjadIDE_git_commit_02():
@@ -64,7 +64,7 @@ def test_AbjadIDE_git_commit_02():
     assert 'Commit message> Updated.' in transcript
     for path in [ide.Path('red_score'), ide.Path('blue_score')]:
         assert f'{path.wrapper} ...' in transcript
-        assert 'Nothing to commit ...' in transcript
+        assert '> ci' in transcript
 
 
 def test_AbjadIDE_git_commit_03():
@@ -77,5 +77,4 @@ def test_AbjadIDE_git_commit_03():
 
     abjad_ide('ll ci q')
     transcript = abjad_ide.io.transcript
-    root = ide.Path('/Users/trevorbaca/baca')
     assert 'Nothing to commit ...' in transcript
