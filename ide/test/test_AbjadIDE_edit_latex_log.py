7ff488c21d464fb0adbf0180834bac3a24e64398
Trevor Bača <trevor.baca@gmail.com>
2019-02-01 12:12:05 -0600

    Taught Travis more about LilyPond installation.

--- ide/test/test_AbjadIDE_edit_latex_log.py
+++ ide/test/test_AbjadIDE_edit_latex_log.py
@@ -4,17 +4,11 @@ import pytest
 abjad_ide = ide.AbjadIDE(test=True)
 
 
-#@pytest.mark.skipif(
-#    os.environ.get('TRAVIS') == 'true',
-#    reason='Travis-CI does create LaTeX log.'
-#    )
 def test_AbjadIDE_edit_latex_log_01():
 
     abjad_ide('lx q')
     transcript = abjad_ide.io.transcript
     path = abjad_ide.configuration.latex_log_file_path
-    print('BBB', abjad_ide.configuration.configuration_directory)
-    print('CCC', path)
     assert f'Editing {path.trim()} ...' in transcript
 
 
--- ide/test/test_AbjadIDE_edit_lilypond_log.py
+++ ide/test/test_AbjadIDE_edit_lilypond_log.py
@@ -5,17 +5,12 @@ import pytest
 abjad_ide = ide.AbjadIDE(test=True)
 
 
-#@pytest.mark.skipif(
-#    os.environ.get('TRAVIS') == 'true',
-#    reason='Travis-CI does create LilyPond log.'
-#    )
 def test_AbjadIDE_edit_lilypond_log_01():
 
     abjad_ide('lp q')
     transcript = abjad_ide.io.transcript
     path = abjad.abjad_configuration.lilypond_log_file_path
     path = ide.Path(path)
-    print('AAA', path)
     assert f'Editing {path.trim()} ...' in transcript
 
 
