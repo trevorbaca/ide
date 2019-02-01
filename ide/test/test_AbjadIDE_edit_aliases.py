506b680d76e270a992961863673702bb2d0ece1c
Trevor Bača <trevor.baca@gmail.com>
2019-02-01 11:36:11 -0600

    Teaching Travis about aliases.

--- ide/test/test_AbjadIDE_edit_aliases.py
+++ ide/test/test_AbjadIDE_edit_aliases.py
@@ -13,6 +13,9 @@ def test_AbjadIDE_edit_aliases_01():
     abjad_ide('red mm metronome al q')
     transcript = abjad_ide.io.transcript
     path = ide.Path(abjad_ide.configuration.aliases_file_path)
+    print(path)
+    print(abjad_ide.configuration.configuration_directory)
+    print(abjad_ide.configuration.aliases_file_path)
     assert f'Editing {path.trim()} ...' in transcript
 
 
