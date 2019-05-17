a6919f6dfb03b33670969baabbec2a866c813a14
Trevor Bača <trevor.baca@gmail.com>
2019-05-17 13:06:37 -0500

    Black-formatted code.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,8 @@ from pygments.formatters.latex import LatexFormatter
 class CustomLatexFormatter(LatexFormatter):
     def __init__(self, **options):
         super(CustomLatexFormatter, self).__init__(**options)
-        self.verboptions = r'''formatcom=\footnotesize'''
+        self.verboptions = r"""formatcom=\footnotesize"""
+
 
 PygmentsBridge.latex_formatter = CustomLatexFormatter
 
@@ -14,44 +15,44 @@ PygmentsBridge.latex_formatter = CustomLatexFormatter
 
 add_function_parentheses = True
 
-copyright = '2008-2018, Trevor Bača'
+copyright = "2008-2018, Trevor Bača"
 
 exclude_patterns = []
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.graphviz',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx_autodoc_typehints',
-    'uqbar.sphinx.api',
-    'uqbar.sphinx.inheritance',
-    'uqbar.sphinx.style',
-    'abjadext.book.sphinx',
-    ]
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.graphviz",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx_autodoc_typehints",
+    "uqbar.sphinx.api",
+    "uqbar.sphinx.inheritance",
+    "uqbar.sphinx.style",
+    "abjadext.book.sphinx",
+]
 
-master_doc = 'index'
+master_doc = "index"
 
-project = 'Abjad IDE'
+project = "Abjad IDE"
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
-release = ''
+release = ""
 
-source_suffix = '.rst'
+source_suffix = ".rst"
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-version = ''
+version = ""
 
 ### HTML ###
 
 html_domain_indices = False
-html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%b %d, %Y"
 html_show_sourcelink = True
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {}
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
@@ -59,35 +60,35 @@ html_use_index = False
 
 ### HTML HELP ###
 
-htmlhelp_basename = 'AbjadAbjadIDEdoc'
+htmlhelp_basename = "AbjadAbjadIDEdoc"
 
 ### LATEX ###
 
 latex_elements = {
-    'inputenc': r'\usepackage[utf8x]{inputenc}',
-    'utf8extra': '',
-    'papersize': 'a4paper',
-    'pointsize': '10pt',
-    'preamble': r'''
+    "inputenc": r"\usepackage[utf8x]{inputenc}",
+    "utf8extra": "",
+    "papersize": "a4paper",
+    "pointsize": "10pt",
+    "preamble": r"""
     \usepackage{upquote}
     \pdfminorversion=5
     \setcounter{tocdepth}{2}
     \definecolor{VerbatimColor}{rgb}{0.95,0.95,0.95}
     \definecolor{VerbatimBorderColor}{rgb}{1.0,1.0,1.0}
-    ''',
-    }
+    """,
+}
 
 latex_documents = [
     (
-        'index',
-        'AbjadExperimentalPackages.tex',
-        'Abjad Experimental Packages Documentation',
-        'Trevor Bača & Josiah Wolf Oberholtzer',
-        'manual',
-        ),
-    ]
+        "index",
+        "AbjadExperimentalPackages.tex",
+        "Abjad Experimental Packages Documentation",
+        "Trevor Bača & Josiah Wolf Oberholtzer",
+        "manual",
+    )
+]
 
-#latex_use_parts = True
+# latex_use_parts = True
 
 latex_domain_indices = False
 
@@ -95,44 +96,44 @@ latex_domain_indices = False
 
 man_pages = [
     (
-        'index',
-        'abjadide',
-        'Abjad Score PackageManager Documentation',
-        [' Trevor Bača & Josiah Wolf Oberholtzer'],
+        "index",
+        "abjadide",
+        "Abjad Score PackageManager Documentation",
+        [" Trevor Bača & Josiah Wolf Oberholtzer"],
         1,
-        )
-    ]
+    )
+]
 
 ### TEXINFO ###
 
 texinfo_documents = [
     (
-        'index',
-        'AbjadAbjadIDE',
-        'Abjad Score PackageManager Documentation',
-        'Trevor Bača & Josiah Wolf Oberholtzer',
-        'AbjadAbjadIDE',
-        'One line description of project.',
-        'Miscellaneous',
-        ),
-    ]
+        "index",
+        "AbjadAbjadIDE",
+        "Abjad Score PackageManager Documentation",
+        "Trevor Bača & Josiah Wolf Oberholtzer",
+        "AbjadAbjadIDE",
+        "One line description of project.",
+        "Miscellaneous",
+    )
+]
 
 ### EXTENSIONS ###
 
 abjadbook_ignored_documents = ()
-autodoc_member_order = 'groupwise'
-graphviz_dot_args = ['-s32']
-graphviz_output_format = 'svg'
+autodoc_member_order = "groupwise"
+graphviz_dot_args = ["-s32"]
+graphviz_output_format = "svg"
 intersphinx_mapping = {
-    'abjad': ('http://abjad.mbrsi.org', None),
-    'python': ('http://docs.python.org/3.6', None),
-    }
+    "abjad": ("http://abjad.mbrsi.org", None),
+    "python": ("http://docs.python.org/3.6", None),
+}
 
-uqbar_api_title = 'Abjad IDE'
-uqbar_api_source_paths = ['ide']
-uqbar_api_root_documenter_class = 'uqbar.apis.SummarizingRootDocumenter'
-uqbar_api_module_documenter_class = 'uqbar.apis.SummarizingModuleDocumenter'
+uqbar_api_title = "Abjad IDE"
+uqbar_api_source_paths = ["ide"]
+uqbar_api_root_documenter_class = "uqbar.apis.SummarizingRootDocumenter"
+uqbar_api_module_documenter_class = "uqbar.apis.SummarizingModuleDocumenter"
 uqbar_api_member_documenter_classes = [
-    'uqbar.apis.FunctionDocumenter',
-    'uqbar.apis.SummarizingClassDocumenter',
-    ]
+    "uqbar.apis.FunctionDocumenter",
+    "uqbar.apis.SummarizingClassDocumenter",
+]
