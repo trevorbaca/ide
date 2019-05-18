0c23c346d2f63d78af5d84162f455dd86eaae3f3
Trevor Bača <trevor.baca@gmail.com>
2019-05-18 11:36:03 -0500

    Taught conftest.py, pytest.ini about ide namespace.

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import abjad
+import ide
 import pytest
-from abjad import utilities
 
 
 pytest_plugins = ["helpers_namespace"]
 
+
+@pytest.fixture(autouse=True)
+def add_ide(doctest_namespace):
+    doctest_namespace['ide'] = ide
 
 @pytest.fixture(autouse=True)
 def add_libraries(doctest_namespace):
@@ -13,9 +17,8 @@ def add_libraries(doctest_namespace):
     doctest_namespace["Infinity"] = abjad.mathtools.Infinity()
     doctest_namespace["NegativeInfinity"] = abjad.mathtools.NegativeInfinity()
 
-
 @pytest.helpers.register
 def list_all_ide_classes(ignored_classes=None):
-    return utilities.list_all_classes(
+    return abjad.utilities.list_all_classes(
         modules="ide", ignored_classes=ignored_classes
     )
