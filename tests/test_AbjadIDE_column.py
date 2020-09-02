ab2e88d8780f403712eae9704a0602dc25037d06
Trevor Bača <trevor.baca@gmail.com>
2020-09-02 16:17:02 -0400

    Unskipped column test.

--- tests/test_AbjadIDE_column.py
+++ tests/test_AbjadIDE_column.py
@@ -1,33 +1,22 @@
-import pytest
-
 import ide
 
 abjad_ide = ide.AbjadIDE(test="dimensions=(10, 54)")
 
 
-@pytest.mark.skip(reason="will fix soon")
 def test_AbjadIDE_column_01():
 
     with ide.Test():
 
-        abjad_ide(
-            """
-            red ee
-            new test-file-01.txt y
-            new test-file-02.txt y
-            new test-file-03.txt y
-            new test-file-04.txt y
-            new test-file-05.txt y
-            new test-file-06.txt y
-            new test-file-07.txt y
-            new test-file-08.txt y
-            new test-file-09.txt y
-            new test-file-10.txt y
-            new test-file-11.txt y
-            new test-file-12.txt y
-            <return>
-            q"""
-        )
+        directory = ide.configuration.test_scores_directory / "red_score" / "red_score"
+        directory /= "etc"
+
+        for i in range(12):
+            i += 1
+            name = f"test-file-{str(i).zfill(2)}.txt"
+            path = directory / name
+            path.touch()
+
+        abjad_ide("red ee q")
         transcript = abjad_ide.io.transcript
         for line in [
             "   1: notes.txt               8: test-file-07.txt",
